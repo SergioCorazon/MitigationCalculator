5ea714cee4daddd9d3872de96936e8ff5a8f8caf
aryalith <chiara2mf@gmail.com>
2023-08-03 16:08:04 +0200

    Added the dmg comparision with the mitigations.

---
+++ MitigationCalculator/Models/Attack.cs
@@ -0,0 +1,14 @@
+﻿namespace MitigationCalculator.Models
+{
+    public class Attack
+    {
+        public Attack(string _Name) { this.Name = _Name; }
+        public string Name { get; set; }
+        public int BossMagicDamage { get; set; }
+
+        public int BossPhysicalDamage { get; set; }
+
+        public int BossFlatDamage { get; set; }
+
+    }
+}
--- MitigationCalculator/Models/Timeline.cs
+++ MitigationCalculator/Models/Timeline.cs
@@ -3,5 +3,17 @@
     public class Timeline
     {
 
+        //Name of the fight
+        //Time
+        //Attacks
+        //Mitigation of the attacks
+        //Boolean that tells you if you survived
+        public string BossName { get; set; }
+        public int EnrageTime { get; set; }
+        public IList<Attack> ListOfAttacks { get; set; }
+
+        public 
+
+
     }
 }
