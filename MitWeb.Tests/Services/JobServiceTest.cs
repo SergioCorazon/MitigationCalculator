bb213d7197d703154c6954f55fae56d5467bfd7c
SergioCorazon <sergioytal@gmail.com>
2024-02-29 10:33:13 +0100

    first implementation of changes

---
+++ MitWeb.Tests/Services/JobServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MITWeb.Models;
+using MITWeb.Services;
+
+namespace MitWeb.Tests.Services
+{
+    public class JobServiceTest
+    {
+        [Fact]
+        public void TestGetJobs()
+        {
+            IJobService js = new JobService();
+            IList<Job> jobList = js.GetJobs();
+            Assert.NotEmpty(jobList);
+        }
+    }
+}
